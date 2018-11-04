99baec7f5ceadf2ac14f8f43a4b9e655ef6c31f8
Julius Tens <mail@juliustens.eu>
2018-11-04 19:54:39 +0100

    soothe the linter

--- test.js
+++ test.js
@@ -21,31 +21,31 @@ tape('lvb.stations', async t => {
 })
 
 const isDNB = (s) =>
-	s.name === 'Leipzig, Deutsche Nationalbibliothek'
-&&	s.id === '11558'
+	s.name === 'Leipzig, Deutsche Nationalbibliothek'	&&
+s.id === '11558'
 
 const isMesse = (s) =>
-	s.name === 'Leipzig, Messegelände'
-&&	s.id === '10818'
+	s.name === 'Leipzig, Messegelände'	&&
+s.id === '10818'
 
 const isStation = (s) =>
-	s.type === 'station'
-&&	isObject(s.coordinates)
-&&	s.coordinates.longitude > 0
-&&	s.coordinates.latitude > 0
+	s.type === 'station'	&&
+isObject(s.coordinates)	&&
+s.coordinates.longitude > 0	&&
+s.coordinates.latitude > 0
 
 const isLine = (l) =>
-	isString(l.id) && l.id.length > 0
-&&	isString(l.class) && l.class.length > 0
-&&	isString(l.direction) && l.direction.length > 0
-&&	isString(l.operator) && l.operator.length > 0
-&&	isString(l.color) && l.color.length === 7
+	isString(l.id) && l.id.length > 0	&&
+isString(l.class) && l.class.length > 0	&&
+isString(l.direction) && l.direction.length > 0	&&
+isString(l.operator) && l.operator.length > 0	&&
+isString(l.color) && l.color.length === 7
 
 const isFare = (f) =>
-	f.type === 'fare'
-&&	isString(f.model) && f.model.length > 0
-&&	isNumber(f.amount) && f.amount > 0
-&&	f.currency === 'EUR'
+	f.type === 'fare'	&&
+isString(f.model) && f.model.length > 0	&&
+isNumber(f.amount) && f.amount > 0	&&
+f.currency === 'EUR'
 
 tape('lvb.journeys', async t => {
 	// DNB to Messegelände
@@ -56,7 +56,7 @@ tape('lvb.journeys', async t => {
 	t.ok(journey.id, 'journey id')
 
 	t.ok(isDNB(journey.legs[0].origin), 'journey origin')
-	t.ok(isMesse(journey.legs[journey.legs.length-1].destination), 'journey destination')
+	t.ok(isMesse(journey.legs[journey.legs.length - 1].destination), 'journey destination')
 
 	const leg = journey.legs[0]
 	t.ok(isStation(leg.origin), 'journey leg origin')
@@ -69,7 +69,7 @@ tape('lvb.journeys', async t => {
 	t.ok(leg.route.length >= 2, 'journey leg route length')
 	t.ok(leg.route.every(isStation), 'journey leg route')
 	t.ok(leg.route[0].id === leg.origin.id, 'journey leg route:first id')
-	t.ok(leg.route[leg.route.length-1].id === leg.destination.id, 'journey leg route:last id')
+	t.ok(leg.route[leg.route.length - 1].id === leg.destination.id, 'journey leg route:last id')
 
 	t.ok(journey.price.currency === 'EUR', 'journey price currency')
 	t.ok(journey.price.amount >= 2, 'journey price amount')
@@ -80,11 +80,11 @@ tape('lvb.journeys', async t => {
 })
 
 const isDepLine = (l) =>
-	isString(l.id) && l.id.length > 0
-&&	isString(l.class) && l.class.length > 0
-&&	isString(l.direction) && l.direction.length > 0
-&&	isString(l.operator) && l.operator.length > 0
-&&	isString(l.name) && l.name.length > 0
+	isString(l.id) && l.id.length > 0	&&
+isString(l.class) && l.class.length > 0	&&
+isString(l.direction) && l.direction.length > 0	&&
+isString(l.operator) && l.operator.length > 0	&&
+isString(l.name) && l.name.length > 0
 
 tape('lvb.departures', async t => {
 	const departures = await lvb.departures('10818')
